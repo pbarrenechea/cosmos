7977f1c7cde652a6a39d6db9788e8502f548fc79
Miguel <miguel.casas@auth0.com>
2018-05-29 00:10:21 -0500

    enhancement welcome page and welcome-cards

--- src/manage/components/welcome-card.js
+++ src/manage/components/welcome-card.js
@@ -2,17 +2,54 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import { Image, Paragraph } from '@auth0/cosmos'
+import { Image, Heading, Paragraph } from '@auth0/cosmos'
 
 const CardWrapper = styled.div`
   line-height: normal;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  display: grid;
+  height: 125px;
+  grid-template-columns: 67px 1fr;
   justify-content: center;
+  align-items: center;
+  padding: 30px;
+  grid-column-gap: 21px;
+`
+const Head = styled(Heading)`
+  margin: 0px;
+`
+
+const Article = styled(Paragraph)`
+  margin: 0px;
+`
+
+const ImageWrapper = styled.span`
+  height: 66px;
+  width: 66px;
+  border-radius: 50%;
+  background-color: #f6f6f6;
+  padding: px;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+`
+const LightText = styled.span`
+  color: #c4c4c4;
 `
 
 const WelcomeCard = props => (
   <CardWrapper>
-    <Image source={props.imagePath} />
-    <Paragraph>{props.paragraph}</Paragraph>
+    <ImageWrapper>
+      <Image source={props.imagePath} />
+    </ImageWrapper>
+    <div>
+      <Head size={4}>{props.title}</Head>
+      <Article>
+        {props.paragraph}
+        {props.lightText && <LightText>{props.lightText}</LightText>}
+      </Article>
+    </div>
   </CardWrapper>
 )
 
--- src/manage/pages/welcome/index.js
+++ src/manage/pages/welcome/index.js
@@ -1,6 +1,27 @@
-import React from 'react'
-import { PageHeader } from '@auth0/cosmos'
+import React, { Fragment } from 'react'
+import { Button, Heading, PageHeader } from '@auth0/cosmos'
 import WelcomeCard from '../../components/welcome-card'
+import styled from 'styled-components'
+
+import logo from './customers-icon-e6580a660a.svg'
+
+const CardsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 24px;
+  grid-row-gap: 20px;
+`
+
+const SubTitle = styled(Heading)`
+  margin-top: 58px;
+`
+
+const Pagination = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: end;
+  margin-top: 61px;
+`
 
 class WelcomeIndex extends React.Component {
   constructor(props) {
@@ -10,32 +31,46 @@ class WelcomeIndex extends React.Component {
 
   render() {
     return (
-      <div>
+      <Fragment>
         <PageHeader
           title="Getting Started"
           description={{
             text: 'Get started with integrating Auth0 by the following guided tutorial below'
           }}
         />
-        <div>
+        <SubTitle size={2}>What do you want to build?</SubTitle>
+        <CardsContainer>
           <WelcomeCard
-            imagePath="../customers-icon-e6580a660a.svg"
-            paragraph="Single Page Web applications"
+            imagePath={logo}
+            title="Single Page Web Aplications"
+            paragraph="A javaScript front-end app that uses an Api."
+            lightText="Eg: AngularJS + NodeJS"
           />
           <WelcomeCard
-            imagePath="../customers-icon-e6580a660a.svg"
-            paragraph="Single Page Web applications"
+            imagePath={logo}
+            title="Single Page Web Aplications"
+            paragraph="A javaScript front-end app that uses an Api."
+            lightText="Eg: AngularJS + NodeJS"
           />
           <WelcomeCard
-            imagePath="../customers-icon-e6580a660a.svg"
-            paragraph="Single Page Web applications"
+            imagePath={logo}
+            title="Single Page Web Aplications"
+            paragraph="A javaScript front-end app that uses an Api."
+            lightText="Eg: AngularJS + NodeJS"
           />
           <WelcomeCard
-            imagePath="../customers-icon-e6580a660a.svg"
-            paragraph="Single Page Web applications"
+            imagePath={logo}
+            title="Single Page Web Aplications"
+            paragraph="A javaScript front-end app that uses an Api."
+            lightText="Eg: AngularJS + NodeJS"
           />
-        </div>
-      </div>
+        </CardsContainer>
+        <Pagination>
+          <Button size="default" appearance="cta">
+            Next
+          </Button>
+        </Pagination>
+      </Fragment>
     )
   }
 }
